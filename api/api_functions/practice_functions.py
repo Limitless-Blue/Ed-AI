554ddd256dbf2bf24212f8126b7043f2d905dc7b
P.V.Chaitanya <93573686+pvchaitanya8@users.noreply.github.com>
2025-02-17 08:47:49 +0530

    Add TAGS to database

---
+++ api/api_functions/practice_functions.py
@@ -0,0 +1,10 @@
+import json
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+from api.api_functions.common_functions import extract_recommendation_list
+
+
+def practice_page_recommendations():
+    pass
