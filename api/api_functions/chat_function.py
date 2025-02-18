dca25c2a1b91b6df6eb19147322f683c33b35632
P.V.Chaitanya <93573686+pvchaitanya8@users.noreply.github.com>
2025-02-18 23:42:05 +0530

    Add chat_functions Funtionality

---
+++ api/api_functions/chat_function.py
@@ -0,0 +1,25 @@
+from typing import List, Optional, Dict, Any
+
+
+def chat_text_text_socraticAI(
+    user_input: str, conversation_history: List[Dict[str, str]]
+):
+    pass
+
+
+def chat_text_text_Non_socraticAI(
+    user_input: str, conversation_history: List[Dict[str, str]]
+):
+    pass
+
+
+def chat_text_voice_socraticAI(
+    user_audio_file: str, conversation_history: List[Dict[str, str]]
+):
+    pass
+
+
+def chat_text_voice_Non_socraticAI(
+    user_audio_file: str, conversation_history: List[Dict[str, str]]
+):
+    pass
--- api/chat.py
+++ api/chat.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, APIRouter, Body, Query, Path
 from pydantic import BaseModel
 from typing import List, Optional, Dict, Any
+from api.api_functions.chat_function import (
+    chat_text_text_socraticAI,
+    chat_text_text_Non_socraticAI,
+    chat_text_voice_socraticAI,
+    chat_text_voice_Non_socraticAI,
+)
 
 chat_router = APIRouter(prefix="/api/chat", tags=["Chat"])
 
@@ -14,13 +20,26 @@ class TextChatRequest(BaseModel):
 # TODO: Add AI Part
 @chat_router.post("/text")
 async def chat_text(data: TextChatRequest):
-    return {
-        "AI": "AI's text response",
-        "conversationHistory": [
-            {"speaker": "user", "text": "Transcribed user speech"},
-            {"speaker": "AI", "text": "AI's text response"},
-        ],
-    }
+    if data.socraticAI:
+        return chat_text_text_socraticAI(data.user, data.conversationHistory)
+        # return {
+        #     "user": "Transcribed user speech",
+        #     "AI": "AI's text response",
+        #     "conversationHistory": [
+        #         {"speaker": "user", "text": "Transcribed user speech"},
+        #         {"speaker": "AI", "text": "AI's text response"},
+        #     ],
+        # }
+    else:
+        return chat_text_text_Non_socraticAI(data.user, data.conversationHistory)
+        # return {
+        #     "user": "Transcribed user speech",
+        #     "AI": "AI's text response",
+        #     "conversationHistory": [
+        #         {"speaker": "user", "text": "Transcribed user speech"},
+        #         {"speaker": "AI", "text": "AI's text response"},
+        #     ],
+        # }
 
 
 class VoiceChatRequest(BaseModel):
@@ -32,11 +51,23 @@ class VoiceChatRequest(BaseModel):
 # TODO: Add AI Part
 @chat_router.post("/voice")
 async def chat_voice(data: VoiceChatRequest):
-    return {
-        "user": "Transcribed user speech",
-        "AI": "AI's text response",
-        "conversationHistory": [
-            {"speaker": "user", "text": "Transcribed user speech"},
-            {"speaker": "AI", "text": "AI's text response"},
-        ],
-    }
+    if data.socraticAI:
+        return chat_text_voice_socraticAI(data.audioFile, data.conversationHistory)
+        # return {
+        #     "user": "Transcribed user speech",
+        #     "AI": "AI's text response",
+        #     "conversationHistory": [
+        #         {"speaker": "user", "text": "Transcribed user speech"},
+        #         {"speaker": "AI", "text": "AI's text response"},
+        #     ],
+        # }
+    else:
+        return chat_text_voice_Non_socraticAI(data.audioFile, data.conversationHistory)
+        # return {
+        #     "user": "Transcribed user speech",
+        #     "AI": "AI's text response",
+        #     "conversationHistory": [
+        #         {"speaker": "user", "text": "Transcribed user speech"},
+        #         {"speaker": "AI", "text": "AI's text response"},
+        #     ],
+        # }
