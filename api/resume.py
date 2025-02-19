a39e1df49b9361054e47c8769fa7f6d6793325a9
P.V.Chaitanya <93573686+pvchaitanya8@users.noreply.github.com>
2025-02-19 20:34:58 +0530

    Update end_interview_results Funtionality

--- api/resume.py
+++ api/resume.py
@@ -16,14 +16,6 @@ async def analyze_job_match(data: ResumeAnalysisRequest):
         "match": "Good",
         "skillGapAnalysis": """Analysis details...""",
         "salaryInsights": """Insights details...""",
-        "linkedinPeople": [
-            {
-                "name": "Katoro",
-                "url": "https://www.linkedin.com/in/chaitanya-venkata-a5a908212/",
-            },
-            {"name": "person_2", "url": None},
-            {"name": "person_3", "url": None},
-        ],
     }
 
 
@@ -32,14 +24,6 @@ async def analyze_job_match(data: ResumeAnalysisRequest):
 async def generate_cover_letter(data: ResumeAnalysisRequest):
     return {
         "coverLetter": r"API_Endpoint\Temp_Static_data\JobSearchOptimization\CoverLetter.txt"
-    }
-
-
-# TODO: Add AI Part
-@resume_router.post("/generate-talore")
-async def generate_talore_resume(data: ResumeAnalysisRequest):
-    return {
-        "pdfFile": r"API_Endpoint\Temp_Static_data\JobSearchOptimization\generated_resume.pdf"
     }
 
 
@@ -61,30 +45,3 @@ class TaloreChatRequest(BaseModel):
     jobDescription: str
     pdfFile: str
     userMessage: Optional[str] = None
-
-
-# TODO: Add AI Part
-@resume_router.post("/talore-chat")
-async def talore_chat(data: TaloreChatRequest):
-    return {
-        "pdfFile": r"API_Endpoint\Temp_Static_data\JobSearchOptimization\generated_resume.pdf"
-    }
-
-
-# TERMINATED: This endpoint is not used in the application
-@resume_router.get("/previous-cover-letters")
-async def get_previous_cover_letters():
-    return [
-        {
-            "date": "1-03-2024",
-            "result": r"API_Endpoint\Temp_Static_data\JobSearchOptimization\CoverLetter.txt",
-        },
-        {
-            "date": "2-03-2024",
-            "result": r"API_Endpoint\Temp_Static_data\JobSearchOptimization\CoverLetter.txt",
-        },
-        {
-            "date": "3-03-2024",
-            "result": r"API_Endpoint\Temp_Static_data\JobSearchOptimization\CoverLetter.txt",
-        },
-    ]
